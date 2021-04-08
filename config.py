a77df891dd88713184d473b7f8cd8bc3b4415bcd
emills11 <ratdog45@gmail.com>
2021-04-08 19:38:33 -0400

    Removed dependency

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import tweepy
 import logging
 import os
-from decouple import config
 
 logger = logging.getLogger()
 
 def create_api():
     # Keys / tokens are environment variables
-    consumer_key = config("CONSUMER_KEY")
-    consumer_secret = config("CONSUMER_SECRET")
-    access_token = config("ACCESS_TOKEN")
-    access_token_secret = config("ACCESS_TOKEN_SECRET")
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
     # Set auth keys and creates API
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
