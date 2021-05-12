a9fa91d770c9e87a6aa75e529b1f262c386dd928
emills11 <ratdog45@gmail.com>
2021-05-12 15:56:27 -0400

    Fixed WNBA games interference

--- games.py
+++ games.py
@@ -2,6 +2,17 @@ import pandas as pd
 from nba_api.stats.endpoints import leaguegamefinder, leaguestandings
 pd.options.mode.chained_assignment = None  # default='warn'
 
+TEAM_IDS = {'1610612737' : 'Hawks', '1610612738' : 'Celtics', '1610612739' : 'Cavaliers',
+            '1610612740' : 'Pelicans', '1610612741' : 'Bulls', '1610612742' : 'Mavericks',
+            '1610612743' : 'Nuggets', '1610612744' : 'Warriors', '1610612745' : 'Rockets',
+            '1610612746' : 'Clippers', '1610612747' : 'Lakers', '1610612748' : 'Heat',
+            '1610612749' : 'Bucks', '1610612750' : 'Timberwolves', '1610612751' : 'Nets',
+            '1610612752' : 'Knicks', '1610612753' : 'Magic', '1610612754' : 'Pacers',
+            '1610612755' : '76ers', '1610612756' : 'Suns', '1610612757' : 'Trail Blazers',
+            '1610612758' : 'Kings', '1610612759' : 'Spurs', '1610612760' : 'Thunder',
+            '1610612761' : 'Raptors', '1610612762' : 'Jazz', '1610612763' : 'Grizzlies',
+            '1610612764' : 'Wizards', '1610612765' : 'Pistons', '1610612766' : 'Hornets'}
+
 def combine_team_games(df, keep_method='home'):
     '''Combine a TEAM_ID-GAME_ID unique table into rows by game. Slow.
 
@@ -63,9 +74,9 @@ def retrieve_games(date):
     # Only select today's games
     games = games[games.GAME_DATE == date]
 
-    # Drop games that are still in progress
+    # Drop games that are still in progress or are that part of a different league
     for index, row in games.iterrows():
-        if row['WL'] == None:
+        if row['WL'] == None or str(row['TEAM_ID']) not in TEAM_IDS:
             games = games.drop([index])
 
     # Sort games by most recent game id and combine into A/B format,
